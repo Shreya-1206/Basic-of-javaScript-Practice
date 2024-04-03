082da2a545bd25ca7248cf6ea8e0ff06821cbd1c
Shreya <shreyaray.1206@gmail.com>
2024-04-03 21:59:26 +0530

    basics of javaScript

---
+++ numbers.js
@@ -0,0 +1,9 @@
+console.log(20 % 6);
+
+console.log(+true);
+
+const bigInt = BigInt(Number.MAX_SAFE_INTEGER);
+console.log(bigInt + 3n);
+
+const bigInt2 = 190_000_000_000_000_00n;
+console.log(bigInt2 +2n);
