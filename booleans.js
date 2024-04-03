db3d142d401129ec3fc0adaf6ddd1ce4e49c2d09
Shreya <shreyaray.1206@gmail.com>
2024-04-03 22:41:23 +0530

    basics of javaScript

---
+++ booleans.js
@@ -0,0 +1,10 @@
+var isAbove18 = true;
+console.log(typeof isAbove18);
+
+const message = "hey";
+const showPostButton = !!message;
+console.log(`Show Post Button : ${showPostButton}`);
+
+const noOfCharacter = message.length;
+const showEditButton = !!noOfCharacter;
+console.log(`show the edit button ${showEditButton}`)
