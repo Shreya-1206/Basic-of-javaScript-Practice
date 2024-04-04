1788a265d7e86f65fc7046be1d0ff0349c61b94d
Shreya <shreyaray.1206@gmail.com>
2024-04-04 14:39:24 +0530

    basics of javaScript

---
+++ toStringToNumber.js
@@ -0,0 +1,11 @@
+const val1 =90;
+const val2 = 98;
+console.log(val1 + val2); // 9098
+
+console.log(`Using number FUNCTION ${val1 + Number(val2)}`);
+
+
+const v1 = 2;
+const v2 =2;
+
+console.log(`${String(v1)} * ${String(v2)} = ${v1*v2}`);
